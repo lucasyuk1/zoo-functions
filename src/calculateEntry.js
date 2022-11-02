7ba5c6400d6699e8a8f636644172e63d265be58f
LucasCS <lcsvdk@gmail.com>
2022-11-02 12:38:43 -0300

    Requisito 8: Implemente a função countAnimals.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui teste
+  if (typeof entrants === 'undefined' || Object.entries(entrants).length === 0) return 0;
+  const { child, adult, senior } = countEntrants(entrants);
+  return child * data.prices.child + adult * data.prices.adult + senior * data.prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
